1c817d61c80c6f30ef03cc70d67c7292e69f3ad1
蒋潜 <2642217208@qq.com>
2019-10-13 22:18:33 +0800

    ”Myweather”

---
+++ myweather/src/main/java/com/bookcode/entity/WeatherResponse.java
@@ -0,0 +1,29 @@
+package com.bookcode.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+public class WeatherResponse implements Serializable {
+
+
+
+    /**
+     * 消息数据
+     */
+    private Weather data;
+
+    /**
+     * 消息状态
+     */
+    private String status;
+
+    /**
+     * 消息描述
+     */
+    private String desc;
+
+
+}
